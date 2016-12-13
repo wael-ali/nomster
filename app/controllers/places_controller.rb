ad2884a001a520956dfc16b12535b731a4985d9f
wael ali <waelali595@gmail.com>
2016-12-13 03:19:37 +0100

    test.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -14,7 +14,7 @@ class PlacesController < ApplicationController
   end
 
   def create
-   
+   / Place.create(place_params)/
    current_user.places.create(place_params)
     redirect_to root_path
     
