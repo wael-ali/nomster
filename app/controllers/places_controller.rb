83fac3e0a3f75ed37af9fe98f1e958ca0c45d18c
wael ali <waelali595@gmail.com>
2016-12-14 01:11:40 +0100

    creating destroy butten+Action

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -37,7 +37,12 @@ class PlacesController < ApplicationController
     @place.update_attributes(place_params)
     redirect_to root_path
 
+  end
 
+  def destroy
+    @place = Place.find(params[:id])
+    @place.destroy
+    redirect_to root_path
     
   end
 
