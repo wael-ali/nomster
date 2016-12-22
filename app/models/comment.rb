7ba351b9d5d6ac569b19640e092b55be6f0e6f3c
wael ali <waelali595@gmail.com>
2016-12-22 02:05:25 +0100

    humanizing the rating

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,18 @@ class Comment < ApplicationRecord
 
   belongs_to :user
   belongs_to :place
-  
+
+   RATINGS = {
+    'one star' => '1_star',
+    'two stars' => '2_stars',
+    'three stars' => '3_star',
+    'four stars' => '4_stars',
+    'five stars' => '5_stars'
+   }
+
+   def humanized_rating
+
+    RATINGS.invert[self.rating]
+     
+   end
 end
