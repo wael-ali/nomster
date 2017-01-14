de95776fdfab6b178e3a5a6d8e8db5efed4dd617
wael ali <waelali595@gmail.com>
2017-01-14 22:50:30 +0100

    creating notificationmailer+mailer.html.erb+mailer.text.erb

---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,12 @@
+class NotificationMailer < ApplicationMailer
+
+  default from: "no-reply@nomsterapp.com"
+
+def comment_added
+
+  mail(to: "waelali595@gmail.com", 
+    subject: "A comment has been added to your place")
+
+end
+
+end
